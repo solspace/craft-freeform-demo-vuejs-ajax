dc340ee87ca56fbb6371cb6674ffff789dd5a8c5
kjmartens <kelsey@solspace.com>
2023-06-09 12:33:08 -0500

    Updating form styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import QuoteForm from './QuoteForm.vue'
+import QuoteForm from './QuoteForm.vue';
 </script>
 
 <template>
-  <main class="flex min-h-screen flex-col items-center justify-center p-24">
-      <h2 class="mb-1 text-lg font-semibold text-left">Freeform 4.x for Craft</h2>
-      <h1 class="mb-5 pb-2 text-2xl font-semibold text-left">VueJS + AJAX demo</h1>
-      <QuoteForm />
-  </main>
+    <main class="flex min-h-screen flex-col items-left justify-left p-5">
+        <h2 class="mb-1 text-lg font-semibold text-left">Freeform 4 for Craft 4</h2>
+        <h1 class="mb-5 pb-2 text-2xl font-semibold text-left">Vue.js + AJAX demo</h1>
+        <QuoteForm />
+    </main>
 </template>
 
 <style scoped>
